181b8692b32281c87f217c6ec91a941852850551
Maxim Siebert <maxim.siebert@gmail.com>
2017-07-31 18:41:20 -0400

    fix: cleanup, feat: homepage structure, nav

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,24 @@
-// JS Goes here - ES6 supported
+// Navigation
+const navButton = document.getElementsByClassName('nav-button')[0],
+      nav = document.getElementById('nav'),
+      body = document.getElementsByTagName('body')[0];
+
+navButton.addEventListener("click", openNav);
+
+function openNav() {
+  if (body.classList.contains('open')) {
+    body.classList.remove('open');
+  } else {
+    body.classList.add('open');
+  }
+}
+
+// Homepage Swiper
+const swiper = new Swiper('.swiper-container', {
+  keyboardControl: true,
+  a11y: true,
+  mousewheelControl: true,
+  hashnav: true,
+  speed: 2000,
+  // freeMode: true
+});
--- src/js/cms.js
+++ src/js/cms.js
@@ -8,12 +8,14 @@ import ProductsPreview from "./cms-preview-templates/products";
 // Example of creating a custom color widget
 class ColorControl extends React.Component {
   render() {
-    return <input
+    return (
+      <input
         style={{height: "80px"}}
         type="color"
         value={this.props.value}
         onInput={(e) => this.props.onChange(e.target.value)}
-    />;
+      />
+    )
   }
 }
 
