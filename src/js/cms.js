cdda13e6654d215902466dcd5188d98f5e4d72c4
Heather <heather@Admins-MacBook-Pro-3.local>
2017-08-07 19:16:10 -0400

    Upload example video of issue with cms

--- src/js/cms.js
+++ src/js/cms.js
@@ -79,4 +79,57 @@ CMS.registerEditorComponent({
   }
 });
 
+// 2-up Module
+CMS.registerEditorComponent({
+  id: "two_up",
+  label: "Two-Up Images",
+  fields: [{name: 'image-one', label: 'First Image', widget: 'object', fields: [
+    {name: 'image', label: 'Image', widget: 'image'},
+    {name: 'alt', label: 'Alt text', widget: 'string'}
+  ]}, {name: 'image-two', label: 'Second Image', widget: 'object', fields: [
+    {name: 'image', label: 'Image', widget: 'image'},
+    {name: 'alt', label: 'Alt text', widget: 'string'}
+  ]}],
+  pattern: /two_up (\S+)\s/,
+  fromBlock: function(match) {
+    return {
+      id: match[1]
+    };
+  },
+  toBlock: function(obj) {
+    return (`<div></div>`);
+  },
+  toPreview: function(obj) {
+    return (`<div></div>`);
+  }
+});
+
+// 3-up Module
+CMS.registerEditorComponent({
+  id: "three_up",
+  label: "Three-Up Images",
+  fields: [{name: 'image-one', label: 'First Image - Portrait', widget: 'object', fields: [
+    {name: 'image', label: 'Image', widget: 'image'},
+    {name: 'alt', label: 'Alt text', widget: 'string'}
+  ]}, {name: 'image-two', label: 'Second Image', widget: 'object', fields: [
+    {name: 'image', label: 'Image', widget: 'image'},
+    {name: 'alt', label: 'Alt text', widget: 'string'}
+  ]}, {name: 'image-three', label: 'Third Image', widget: 'object', fields: [
+    {name: 'image', label: 'Image', widget: 'image'},
+    {name: 'alt', label: 'Alt text', widget: 'string'}
+  ]}],
+  pattern: /three_up (\S+)\s/,
+  fromBlock: function(match) {
+    return {
+      id: match[1]
+    };
+  },
+  toBlock: function(obj) {
+    return (`<div></div>`);
+  },
+  toPreview: function(obj) {
+    return (`<div></div>`);
+  }
+});
+
 CMS.registerPreviewStyle("/css/main.css");
