41a504ced3ef47b51f4113d9d21102e2b173a658
Heather <heather@Admins-MacBook-Pro-3.local>
2017-08-07 15:09:59 -0400

    Test adding custom widget to CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,24 +1,10 @@
-import React from "react";
 import CMS from "netlify-cms";
 
 import PostPreview from "./cms-preview-templates/post";
 import ProductsPreview from "./cms-preview-templates/products";
+import { SlidesControl, SlidesPreview } from "./Slides";
 
-
-// Example of creating a custom color widget
-class ColorControl extends React.Component {
-  render() {
-    return (
-      <input
-        style={{height: "80px"}}
-        type="color"
-        value={this.props.value}
-        onInput={(e) => this.props.onChange(e.target.value)}
-      />
-    )
-  }
-}
-
+CMS.registerWidget("slides", SlidesControl, SlidesPreview);
 CMS.registerPreviewStyle("/css/main.css");
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
