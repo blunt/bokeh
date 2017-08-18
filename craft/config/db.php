df25c8cd9ab880cf660de93232e339cf79512569
Maxim Siebert <maxim.siebert@gmail.com>
2017-08-17 21:23:36 -0400

    first studio slide, ftpconfig file, htpassword

--- craft/config/db.php
+++ craft/config/db.php
@@ -11,15 +11,31 @@ return array(
 
 	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
 	'server' => 'localhost',
+	// 'server' => '127.0.0.1',
 
 	// The name of the database to select.
+
+	// Local
 	'database' => 'bokeh',
 
+	// Live
+	// 'database' => 'bokeh_main',
+
 	// The database username to connect with.
+
+	//Local
 	'user' => 'root',
 
+	//Live
+	// 'user' => 'bokeh_admin',
+
 	// The database password to connect with.
+
+	//Local
 	'password' => 'root',
+
+	//Live
+	// 'password' => 'dcK4pKqU/D3',
 
 	// The prefix to use when naming tables. This can be no more than 5 characters.
 	'tablePrefix' => 'craft',
